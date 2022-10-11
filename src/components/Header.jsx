51c710686209d99ae908919fa6f9554f90a9234c
Rodrigo Enrique Alfaro <rodrigoenriquealfaro@hotmail.com>
2022-10-11 18:31:14 -0300

    Front-end páginas signIn, SignUp y ForgotPassword creadas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,9 +23,15 @@ const Header = () => {
                 </div>
                 <div>
                     <ul className='flex space-x-10'>
-                        <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/") && 'text-black border-b-red-500'}`} onClick={() => navigate('/')}>Home</li>
-                        <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/offers") && 'text-black border-b-red-500'}`} onClick={() => navigate('/offers')}>Offers</li>
-                        <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/sign-in") && 'text-black border-b-red-500'}`} onClick={() => navigate('/sign-in')}>Sign In</li>
+                        <li
+                            className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/") && 'text-black border-b-red-500'}`}
+                            onClick={() => navigate('/')}>Home</li>
+                        <li
+                            className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/offers") && 'text-black border-b-red-500'}`}
+                            onClick={() => navigate('/offers')}>Offers</li>
+                        <li
+                            className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/sign-in") && 'text-black border-b-red-500'}`}
+                            onClick={() => navigate('/sign-in')}>Sign In</li>
                     </ul>
                 </div>
             </header>
